9cf717b55a5965e90832f92c18886fca8851cd04
detrayV982 <detrayv@uw.edu>
2026-01-26 15:17:56 -0800

    rebase

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,8 +10,19 @@ ASSET_MANAGER.downloadAll(() => {
     const canvas = document.getElementById("gameWorld");
     const ctx = canvas.getContext("2d");
 
-    gameEngine.addEntity(new Platform(0, 300, 300, 20));
+
+   
+	gameEngine.addEntity(new Platform(0, 734, 1024, 1000));
+	gameEngine.addEntity(new Spikes(0, 723, 1024, 1000));
+
+    gameEngine.addEntity(new Platform(0, 500, 200, 20));
+    gameEngine.addEntity(new Platform(250, 400, 125, 20));
+    gameEngine.addEntity(new Platform(400, 550, 75, 20));
+    gameEngine.addEntity(new Platform(600, 650, 75, 20));
+    gameEngine.addEntity(new Platform(725, 550, 75, 20));
+    gameEngine.addEntity(new Platform(874, 450, 150, 20));
     gameEngine.addEntity(new Platform(0, 736, 1024, 50));
+
     gameEngine.addEntity(new Caveman(gameEngine));
 
     gameEngine.init(ctx);
