a74a03220829d162ea21128423e1a0176bc353aa
Saeed Esparza <saeednesp@gmail.com>
2026-01-26 15:08:58 -0800

    Implemented spike.js and spikes to ground for tutorial level.

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,6 +3,7 @@ const ASSET_MANAGER = new AssetManager();
 
 
 ASSET_MANAGER.queueDownload("./Assets/blocks_prev.png");
+ASSET_MANAGER.queueDownload("./Assets/Spikes.png");
 ASSET_MANAGER.queueDownload("./Assets/spritesheet_caveman.png");
 
 ASSET_MANAGER.downloadAll(() => {
@@ -10,7 +11,8 @@ ASSET_MANAGER.downloadAll(() => {
     const ctx = canvas.getContext("2d");
 
     gameEngine.addEntity(new Platform(0, 300, 300, 20));
-    gameEngine.addEntity(new Platform(0, 736, 1024, 50));
+	gameEngine.addEntity(new Platform(0, 734, 1024, 1000));
+	gameEngine.addEntity(new Spikes(0, 723, 1024, 1000));
     gameEngine.addEntity(new Caveman(gameEngine));
 
     gameEngine.init(ctx);
---
+++ Scripts/spikes.js
@@ -0,0 +1,31 @@
+class Spikes {
+    constructor(x, y, width, height, tileIndex = 0) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.tileSize = 15;
+        this.tileIndex = tileIndex;
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Spikes.png");
+    }
+
+    update() {
+
+    }
+
+    draw(ctx) {
+        const tilesAcross = Math.ceil(this.width / this.tileSize);
+
+        for (let i = 0; i < tilesAcross; i++) {
+            ctx.drawImage(
+                this.spritesheet,
+                this.tileIndex * this.tileSize, 0,
+                this.tileSize, this.tileSize,
+                this.x + i * this.tileSize, this.y,
+                this.tileSize, this.tileSize
+            );
+        }
+    }
+}
