e1b7a1324e2f4ab3deaadc62c071a4d2357d342f
detrayV982 <vaughn.detray@outlook.com>
2026-01-28 17:10:25 -0800

    Revert "platform fix"
    This reverts commit c0b1f0d0793ed21cb49d18f43b42013e280685c8.

--- Scripts/main.js
+++ Scripts/main.js
@@ -24,8 +24,8 @@ ASSET_MANAGER.downloadAll(() => {
     gameEngine.addEntity(new Platform(874, 450, 150, 32));
     gameEngine.addEntity(new Platform(0, 736, 1024, 32));
 
-	gameEngine.addEntity(new Platform(250, 296, 125, 32));
-	gameEngine.addEntity(new SpikesUD(254, 326, 120, 11));
+	gameEngine.addEntity(new Platform(250, 300, 125, 32));
+	gameEngine.addEntity(new SpikesUD(254, 330, 120, 11));
 
     gameEngine.addEntity(new Caveman(gameEngine));
 
